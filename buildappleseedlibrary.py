2815cb191b37a772ef5bbc098df3b158988101b7
Sergo <usakhelo@users.noreply.github.com>
2019-08-21 00:54:02 -0400

    Fix port types and default values, add outputs, nodedef types

--- buildappleseedlibrary.py
+++ buildappleseedlibrary.py
@@ -13,12 +13,21 @@ sys.path.append('C:\\plugins\\appleseed\\appleseed\\sandbox\\lib\\vc140\\Release
 os.environ['PATH'] += os.pathsep + 'C:\\plugins\\appleseed\\appleseed\\sandbox\\bin\\vc140\\Release'
 import appleseed as asr
 
+oso_shaders_dir = 'C:\\plugins\\appleseed\\appleseed\\sandbox\\shaders\\appleseed'
 
+def check_multioutput(query):
+    
+    num_outputs = 0
+    num_params = query.get_num_params()
+    for i in range(0, num_params):
+        pinfo = query.get_param_info(i)
+        if pinfo.get('isoutput'):
+            num_outputs += 1
+        if num_outputs > 1:
+            break
+    return num_outputs > 1
 
-oso_shaders_dir = "C:\\plugins\\appleseed\\appleseed\\sandbox\\shaders\\appleseed"
-
-
-def add_inputs(nodeDef, query):
+def add_ports(nodeDef, query):
 
     num_params = query.get_num_params()
     for i in range(0, num_params):
@@ -26,13 +35,42 @@ def add_inputs(nodeDef, query):
 
         pname = pinfo.get('name', '')
         ptype = pinfo.get('type', '')
-        pvalue = pinfo.get('value', '')
-        # plabel = pinfo.get('label', '')
-        # ppage = pinfo.get('page', '')
+        pvalue = None
 
-        print pname, ptype, pvalue
-        def_input = nodeDef.addInput(name=pname, type=ptype)
-        def_input.setValue(pvalue)
+        if pinfo.get('validdefault'):
+            pvalue = pinfo.get('default')
+
+        if pinfo.get('isclosure') and ptype == 'pointer':
+            ptype = 'surfaceshader'
+        elif ptype == 'point' or ptype == 'normal' or ptype == 'vector':
+            ptype = 'vector3'
+        elif ptype == 'color':
+            ptype = 'color3'
+        elif ptype == 'int':
+            ptype = 'integer'
+        elif ptype == 'matrix':
+            ptype = 'matrix44'
+        elif pinfo.get('isarray'):
+            if ptype.startswith('float'):
+                ptype = 'floatarray'
+            elif ptype.startswith('int'):
+                ptype = 'integerarray'
+            elif ptype.startswith('color'):
+                ptype = 'color3rarray'
+            elif ptype.startswith('point'):
+                ptype = 'vector3rarray'
+        
+        if ptype != 'string' and pvalue is not None:
+            pvalue = ', '.join(str(pvalue).split(' '))
+
+        if pinfo.get('isoutput'):
+            def_output = nodeDef.addOutput(name=pname, type=ptype)
+            if pvalue is not None:
+                def_output.setValue(pvalue, ptype)
+        else:
+            def_input = nodeDef.addInput(name=pname, type=ptype)
+            if pvalue is not None:
+                def_input.setValue(pvalue, ptype)
 
 
 def generate_appleseed_nodedefs():
@@ -50,15 +88,16 @@ def generate_appleseed_nodedefs():
 
     for root, dir, files in os.walk(oso_shaders_dir):
         for file in files:
-            if file.endswith(".oso"):
+            if file.endswith('.oso'):
                 filename = os.path.join(root, file)
                 q.open(filename)
                 name = q.get_shader_name()
                 # Create a nodedef taking three color3 and producing another color3
-                nodeDef = doc.addNodeDef("ND_%s" % name, "surfaceshader", name)  # type to be determined from shader type
-                add_inputs(nodeDef, q)
+                nodeType = 'multioutput' if check_multioutput(q) else 'surfaceshader'
+                nodeDef = doc.addNodeDef('ND_%s' % name, nodeType, name)  # type to be determined from shader type
+                add_ports(nodeDef, q)
 
-    outfile = "appleseed_auto_defs.mtlx"
+    outfile = 'appleseed_auto_defs.mtlx'
     mx.writeToXmlFile(doc, outfile)
 
 if __name__ == '__main__':
